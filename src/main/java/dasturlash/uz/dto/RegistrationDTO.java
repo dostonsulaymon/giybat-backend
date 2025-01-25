252cdc2529d66cbe23e7a5e93649a40ef9e1717d
Doston Sulaymon <dostonsulaymon@Dostons-MacBook-Pro.local>
2025-01-25 20:51:05 +0500

    SpringSecuritConfig class and securit package added

--- src/main/java/dasturlash/uz/dto/RegistrationDTO.java
+++ src/main/java/dasturlash/uz/dto/RegistrationDTO.java
@@ -1,11 +1,13 @@
 package dasturlash.uz.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RegistrationDTO {
+
     private String name;
     private String username;
     private String password;
