4a10d8b16118cd22d303b9c89ee4b51ae4b613cd
Doston Sulaymon <dostonsulaymon@Dostons-MacBook-Pro.local>
2025-01-25 19:45:23 +0500

    lombok muammosi hal bo'ldi

--- src/main/java/dasturlash/uz/entity/ProfileEntity.java
+++ src/main/java/dasturlash/uz/entity/ProfileEntity.java
@@ -14,13 +14,13 @@ import java.time.LocalDateTime;
 public class ProfileEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(name = "name")
-    private String username;
+    private String name;
 
     @Column(name = "username")
-    private String name;
+    private String username;
 
     @Column(name = "password")
     private String password;
@@ -30,74 +30,12 @@ public class ProfileEntity {
     private GeneralStatus status; //ACTIVE, BLOCK
 
     @Column(name = "visible")
-    private boolean visible = Boolean.TRUE;
+    private Boolean visible = Boolean.TRUE;
 
     @Column(name = "created_date")
     private LocalDateTime createdDate;
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public GeneralStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(GeneralStatus status) {
-        this.status = status;
-    }
-
-    public boolean isVisible() {
-        return visible;
-    }
-
-    public void setVisible(boolean visible) {
-        this.visible = visible;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-
-    // ROLE, USER, ADMIN
-
-//    @OneToMany(mappedBy = "profile")
-//    private List<ProfileRoleEntity> rolesList;
-
-
-
-
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
 }
